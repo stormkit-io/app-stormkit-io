2f16e34f5e972b76a4181aba6a60866b23e8c7e8
Savas Vedova <savas@stormkit.io>
2020-05-27 22:57:52 +0300

    Handle refs and secondary design

---
+++ src/components/Link/Link.js
@@ -0,0 +1,44 @@
+import React, { forwardRef } from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import { Link as OriginalLink } from "react-router-dom";
+
+const Link = forwardRef(
+  ({ to, children, secondary, className, ...props }, ref) => {
+    let isExternal = false;
+    let classes = className;
+
+    if (secondary) {
+      classes = cn(className, "text-pink-50", "hover:text-secondary");
+    }
+
+    if (to.indexOf("http") === 0 || to.indexOf("//") === 0) {
+      props.rel = "noreferrer noopener";
+      props.target = "_blank";
+      isExternal = true;
+    }
+
+    if (isExternal) {
+      return (
+        <a href={to} {...props} ref={ref} className={classes}>
+          {children}
+        </a>
+      );
+    }
+
+    return (
+      <OriginalLink to={to} {...props} className={classes} ref={ref}>
+        {children}
+      </OriginalLink>
+    );
+  }
+);
+
+Link.propTypes = {
+  to: PropTypes.string,
+  children: PropTypes.node,
+  className: PropTypes.any,
+  secondary: PropTypes.bool,
+};
+
+export default Link;
