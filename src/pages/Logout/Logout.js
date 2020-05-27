5b4ccec4e199004b742a87e576fbcfadae6d6300
Savas Vedova <savas@stormkit.io>
2020-05-27 12:26:55 +0300

    Create the logout page

---
+++ src/pages/Logout/Logout.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import PropTypes from "prop-types";
+import AuthContext from "~/pages/Auth/Auth.context";
+import { connect } from "~/utils/context";
+
+const Logout = ({ logout }) => {
+  useEffect(logout, []);
+  return null;
+};
+
+Logout.propTypes = {
+  logout: PropTypes.func,
+};
+
+export default connect(Logout, [{ Context: AuthContext, props: ["logout"] }]);
