d1f18ae3f136a5c23fa17f68783b025424dae7f8
Savas Vedova <savas@stormkit.io>
2020-06-23 14:17:42 +0300

    Add possibility to use as a popover

--- src/components/ExplanationBox/ExplanationBox.js
+++ src/components/ExplanationBox/ExplanationBox.js
@@ -1,18 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cn from "classnames";
+import "./ExplanationBox.css";
 
-const ExplanationBox = ({ title, children }) => {
+const ExplanationBox = ({
+  title,
+  children,
+  className,
+  absolute,
+  arrowStyles,
+}) => {
   return (
-    <section className="bg-blue-20 text-secondary p-8 rounded-lg">
+    <section
+      className={cn(
+        "bg-blue-20 text-secondary p-8 rounded",
+        { "absolute z-10": absolute },
+        className
+      )}
+    >
+      {absolute && <div class="exp-arrow-up" style={arrowStyles} />}
       <h3 className="font-bold text-lg mb-4">{title}</h3>
-      <div className="leading-6">{children}</div>
+      <div className="leading-6 text-sm">{children}</div>
     </section>
   );
+};
+
+ExplanationBox.defaultProps = {
+  arrowStyles: {
+    left: "50%",
+    transform: "translateX(-50%)",
+  },
 };
 
 ExplanationBox.propTypes = {
   title: PropTypes.string,
   children: PropTypes.node,
+  className: PropTypes.any,
+  absolute: PropTypes.bool,
+  arrowStyles: PropTypes.object,
 };
 
 export default ExplanationBox;
