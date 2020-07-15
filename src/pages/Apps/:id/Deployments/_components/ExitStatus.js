380c488efc13225cacc2e7352557736d4e29694c
Savas Vedova <savas@stormkit.io>
2020-07-16 00:34:10 +0300

    Pass down rest of the properties

--- src/pages/Apps/:id/Deployments/_components/ExitStatus.js
+++ src/pages/Apps/:id/Deployments/_components/ExitStatus.js
@@ -2,17 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import cn from "classnames";
 
-const ExitStatus = ({ code, iconOnly, className }) =>
+const ExitStatus = ({ code, iconOnly, className, ...rest }) =>
   code === 0 ? (
-    <span className={cn("text-green-50", className)}>
+    <span className={cn("text-green-50", className)} {...rest}>
       <span className="fas fa-check-circle" /> {!iconOnly && "Success"}
     </span>
   ) : code !== null ? (
-    <span className={cn("text-red-50", className)}>
+    <span className={cn("text-red-50", className)} {...rest}>
       <span className="fas fa-times-circle" /> {!iconOnly && "Failed"}
     </span>
   ) : (
-    <span className={cn("text-blue-40", className)}>
+    <span className={cn("text-blue-40", className)} {...rest}>
       <span className="fas fa-running" /> {!iconOnly && "Running"}
     </span>
   );
