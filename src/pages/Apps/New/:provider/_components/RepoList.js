6a85ed09b8901a83d7897a35e10fe99a850321ec
Julian Derungs <julian.derungs@primesystems.ch>
2020-09-12 17:43:08 +0200

    feat: Fade in loaded repository list items
    Fade in additionally loaded repositories.
    This enhances the user experience while paginating through the repos.
    
    Relates to: #14

--- src/pages/Apps/New/:provider/_components/RepoList.js
+++ src/pages/Apps/New/:provider/_components/RepoList.js
@@ -5,6 +5,7 @@ import InfoBox from "~/components/InfoBox";
 import Button from "~/components/Button";
 import Spinner from "~/components/Spinner";
 import { insertRepo } from "../actions";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const RepoList = ({
   api,
@@ -33,45 +34,54 @@ const RepoList = ({
         </InfoBox>
       )}
       {repositories.length > 0 ? (
-        repositories.map((r) => (
-          <Button
-            tertiary
-            as="div"
-            styled={false}
-            key={r.id || r.name}
-            className="flex items-center w-full p-4 hover:bg-gray-75 rounded cursor-pointer"
-            onClick={insertRepo({
-              repo: r.full_name || r.path_with_namespace,
-              api,
-              history,
-              provider,
-              setState,
-            })}
-          >
-            <div>
-              <span
-                className={`fab fa-${provider} text-4xl text-${provider} mr-4 rounded-full`}
-                alt={`${provider}-logo`}
-              />
-            </div>
-            <div className="flex-auto text-sm font-bold">{r.name}</div>
-            <div className="uppercase text-xs font-bold text-pink-50 items-center inline-flex">
-              <span
-                className={cn({
-                  invisible:
-                    state.loadingInsert ===
-                    (r.full_name || r.path_with_namespace),
+        <TransitionGroup>
+          {repositories.map((r) => (
+            <CSSTransition
+              timeout={350}
+              classNames="fade-in"
+              unmountOnExit
+              appear
+              key={r.id || r.name}
+            >
+              <Button
+                tertiary
+                as="div"
+                styled={false}
+                className="flex items-center w-full p-4 hover:bg-gray-75 rounded cursor-pointer"
+                onClick={insertRepo({
+                  repo: r.full_name || r.path_with_namespace,
+                  api,
+                  history,
+                  provider,
+                  setState,
                 })}
               >
-                Select
-              </span>
-              {state.loadingInsert ===
-                (r.full_name || r.path_with_namespace) && (
-                <Spinner primary width={6} height={6} />
-              )}
-            </div>
-          </Button>
-        ))
+                <div>
+                  <span
+                    className={`fab fa-${provider} text-4xl text-${provider} mr-4 rounded-full`}
+                    alt={`${provider}-logo`}
+                  />
+                </div>
+                <div className="flex-auto text-sm font-bold">{r.name}</div>
+                <div className="uppercase text-xs font-bold text-pink-50 items-center inline-flex">
+                  <span
+                    className={cn({
+                      invisible:
+                        state.loadingInsert ===
+                        (r.full_name || r.path_with_namespace),
+                    })}
+                  >
+                    Select
+                  </span>
+                  {state.loadingInsert ===
+                    (r.full_name || r.path_with_namespace) && (
+                    <Spinner primary width={6} height={6} />
+                  )}
+                </div>
+              </Button>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
       ) : (
         <div>
           <InfoBox type={InfoBox.ERROR} className="mb-12">
