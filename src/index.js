0fef7e42203e3698984ea1f3d82ae50354d094c1
Julian Derungs <julian.derungs@primesystems.ch>
2020-09-12 17:43:08 +0200

    feat: Fade in loaded repository list items
    Fade in additionally loaded repositories.
    This enhances the user experience while paginating through the repos.
    
    Relates to: #14

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "~/assets/styles/tailwind.css";
 import "~/assets/styles/global.css";
+import "~/assets/styles/transitions.css"
 import RootContext from "~/pages/Root.context";
 import Root from "~/pages/Root";
 
