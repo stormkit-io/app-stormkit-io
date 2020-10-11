20db699e832f2c65b8bce6cf3117142d410c738b
Savas Vedova <savas@stormkit.io>
2020-10-11 10:32:10 +0300

    Refactor: migrate to typescript

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,21 +1,19 @@
-import React, { forwardRef, FC, ReactElement } from "react";
+import React, { forwardRef, ReactElement } from "react";
 import cn from "classnames";
 import { Link as OriginalLink } from "react-router-dom";
 
-interface Props {
-  to: string,
-  children: HTMLElement,
-  className: string,
-  secondary: boolean,
-  tertiary: boolean,
-  rel?: string,
-  target?: string
-}
+type Props = {
+  to: string;
+  secondary?: boolean;
+  tertiary?: boolean;
+} & React.HTMLProps<HTMLAnchorElement>;
 
-const Link: FC<Props> = forwardRef<HTMLAnchorElement, Props>(
-  ({ to, children, secondary, tertiary, className, ...props }: Props, ref): ReactElement => {
+const Link = forwardRef<HTMLAnchorElement, Props>(
+  (props: Props, ref): ReactElement => {
+    const { to, children, secondary, tertiary, className, ...rest } = props;
+
     let isExternal = false;
-    let classes: string = className;
+    let classes: string | undefined = className;
 
     if (secondary) {
       classes = cn(className, "text-pink-50", "hover:text-red-50");
@@ -29,8 +27,8 @@ const Link: FC<Props> = forwardRef<HTMLAnchorElement, Props>(
     }
 
     if (to.indexOf("http") === 0 || to.indexOf("//") === 0) {
-      props.rel = "noreferrer noopener";
-      props.target = "_blank";
+      rest.rel = "noreferrer noopener";
+      rest.target = "_blank";
       isExternal = true;
     } else if (to.indexOf("mailto:") === 0) {
       isExternal = true;
@@ -38,14 +36,14 @@ const Link: FC<Props> = forwardRef<HTMLAnchorElement, Props>(
 
     if (isExternal) {
       return (
-        <a href={to} {...props} ref={ref} className={classes}>
+        <a href={to} {...rest} ref={ref} className={classes}>
           {children}
         </a>
       );
     }
 
     return (
-      <OriginalLink to={to} {...props} className={classes} ref={ref}>
+      <OriginalLink to={to} {...rest} className={classes} ref={ref}>
         {children}
       </OriginalLink>
     );
--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,12 +3,12 @@ import cn from "classnames";
 import "./Spinner.css";
 
 interface Props {
-  width: number,
-  height: number,
-  pageCenter?: boolean, // Whether to center in the middle of the page or not.
-  primary?: boolean,
-  secondary?: boolean,
-  className: string,
+  width?: number;
+  height?: number;
+  pageCenter?: boolean; // Whether to center in the middle of the page or not.
+  primary?: boolean;
+  secondary?: boolean;
+  className?: string;
 }
 
 const Spinner: FC<Props> = ({
@@ -19,25 +19,25 @@ const Spinner: FC<Props> = ({
   secondary,
   className,
 }: Props): ReactElement => (
-    <div
-      className={cn(
-        "spinner",
-        "rounded-full",
-        `w-${width} h-${height}`,
-        {
-          "page-center": pageCenter,
-          "bg-pink-50": primary,
-          "bg-blue-20": secondary,
-        },
-        className
-      )}
-    >
-      <div className="relative w-full h-full">
-        <div className="spinner-bounce w-full h-full rounded-full bg-white absolute top-0 left-0" />
-        <div className="spinner-bounce w-full h-full rounded-full bg-white absolute top-0 left-0" />
-      </div>
+  <div
+    className={cn(
+      "spinner",
+      "rounded-full",
+      `w-${width} h-${height}`,
+      {
+        "page-center": pageCenter,
+        "bg-pink-50": primary,
+        "bg-blue-20": secondary,
+      },
+      className
+    )}
+  >
+    <div className="relative w-full h-full">
+      <div className="spinner-bounce w-full h-full rounded-full bg-white absolute top-0 left-0" />
+      <div className="spinner-bounce w-full h-full rounded-full bg-white absolute top-0 left-0" />
     </div>
-  );
+  </div>
+);
 
 // Width and height are numbers compatible with tailwind sizes.
 Spinner.defaultProps = {
