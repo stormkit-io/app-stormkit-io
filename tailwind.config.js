dce97bb3a6cc92526e98212ca4fb18d651f7871c
Savas Vedova <savas@stormkit.io>
2020-10-31 23:34:30 +0100

    Fix: typescript support for tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === "production",
-    content: ["src/index.js", "src/**/*.js"],
+    content: ["src/index.js", "src/**/*.js", "src/**/*.ts", "src/**/*.tsx"],
     options: {
       whitelist: [
         // Provider related dynamic classes
