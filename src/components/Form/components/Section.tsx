108dc949ca2c8fecbf13e3dfbc4386ae38be65b2
Savas Vedova <savas@stormkit.io>
2020-10-31 22:37:51 +0100

    Fix: make Section props optional

--- src/components/Form/components/Section.tsx
+++ src/components/Form/components/Section.tsx
@@ -2,19 +2,18 @@ import React, { ReactNode, FC, ReactElement } from "react";
 import cn from "classnames";
 
 type Props = {
-  marginBottom: string; // The tailwind class for the marginBottom
+  marginBottom?: string; // The tailwind class for the marginBottom
+  className?: string;
   children: ReactNode;
-  className: string;
   label: ReactNode;
-}
+};
 
 const Section: FC<Props> = ({
   children,
   className,
   label,
-  marginBottom = "mb-8"
+  marginBottom = "mb-8",
 }: Props): ReactElement => {
-
   return (
     <div className={cn("flex", marginBottom, className)}>
       <div className="flex-auto pt-4 min-w-48 max-w-48 font-bold">{label}</div>
