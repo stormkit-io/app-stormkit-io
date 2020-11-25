b79f47316784c1c8f87896cd4d57c314e76ce49a
filippofinke <filippofinke@protonmail.ch>
2020-11-25 15:50:18 +0100

    Now the menu can be closed by clicking out of it.

--- src/layouts/_components/UserMenu.js
+++ src/layouts/_components/UserMenu.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import cn from "classnames";
 import Link from "~/components/Link";
+import OutsideClick from "~/components/OutsideClick";
 
 const menuItems = [
   {
@@ -51,70 +52,72 @@ const UserMenu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav>
-      <button className="p-2" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        <span className="fas fa-bars text-2xl text-secondary hover:text-white" />
-        <span className="hidden">Menu</span>
-      </button>
-      <section
-        role="menu"
-        className={cn(
-          "flex flex-col absolute right-0 shadow-2xl p-8 rounded bg-white border border-black z-10",
-          {
-            hidden: !isMenuOpen
-          }
-        )}
-      >
-        <div className="flex">
-          {menuItems.map(item => (
-            <div className="w-40" key={item.title}>
-              <h3 className="uppercase font-bold text-gray-70 mb-6 text-xs">
-                {item.title}
-              </h3>
-              <ul>
-                {item.children.map(link => (
-                  <li key={link.text}>
-                    <Link
-                      to={link.to}
-                      tertiary
-                      className="mb-6 block uppercase text-xs"
-                    >
-                      {link.text}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-        <div className="border-t border-gray-80 border-solid pt-6 mt-12 leading-relaxed text-center">
-          <p className="text-xl">
-            <a
-              target="_blank"
-              rel="noreferrer nooopener"
-              href="https://twitter.com/stormkitio"
-              className="mr-4"
-            >
-              <span className="fab fa-twitter" />
-            </a>
-            <a
-              target="_blank"
-              rel="noreferrer nooopener"
-              href="https://www.youtube.com/channel/UC6C_-UuAiIWlGOIokT03lRQ"
-            >
-              <span className="fab fa-youtube" />
-            </a>
-          </p>
-          <p className="inline-flex items-center text-xs">
-            &copy; {new Date().getFullYear()} Stormkit, Inc. - all rights
-            reserved.
-          </p>
-          <p className="text-xs">
-            Made with <span className="fas fa-heart text-pink-50" />
-          </p>
-        </div>
-      </section>
-    </nav>
+    <OutsideClick handler={() => setIsMenuOpen(false)}>
+      <nav>
+        <button className="p-2" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <span className="fas fa-bars text-2xl text-secondary hover:text-white" />
+          <span className="hidden">Menu</span>
+        </button>
+        <section
+          role="menu"
+          className={cn(
+            "flex flex-col absolute right-0 shadow-2xl p-8 rounded bg-white border border-black z-10",
+            {
+              hidden: !isMenuOpen
+            }
+          )}
+        >
+          <div className="flex">
+            {menuItems.map(item => (
+              <div className="w-40" key={item.title}>
+                <h3 className="uppercase font-bold text-gray-70 mb-6 text-xs">
+                  {item.title}
+                </h3>
+                <ul>
+                  {item.children.map(link => (
+                    <li key={link.text}>
+                      <Link
+                        to={link.to}
+                        tertiary
+                        className="mb-6 block uppercase text-xs"
+                      >
+                        {link.text}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <div className="border-t border-gray-80 border-solid pt-6 mt-12 leading-relaxed text-center">
+            <p className="text-xl">
+              <a
+                target="_blank"
+                rel="noreferrer nooopener"
+                href="https://twitter.com/stormkitio"
+                className="mr-4"
+              >
+                <span className="fab fa-twitter" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer nooopener"
+                href="https://www.youtube.com/channel/UC6C_-UuAiIWlGOIokT03lRQ"
+              >
+                <span className="fab fa-youtube" />
+              </a>
+            </p>
+            <p className="inline-flex items-center text-xs">
+              &copy; {new Date().getFullYear()} Stormkit, Inc. - all rights
+              reserved.
+            </p>
+            <p className="text-xs">
+              Made with <span className="fas fa-heart text-pink-50" />
+            </p>
+          </div>
+        </section>
+      </nav>
+    </OutsideClick>
   );
 };
 
