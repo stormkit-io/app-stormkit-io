c4cc396f3d624fcfe8549521e6c804ab72b29d9b
Ivan Lori <ivan.lori@protonmail.com>
2020-11-28 11:37:16 +0100

    Refactor: migration of modal component to TS
    @svedova additionally I've changed the way in which some boolean variables was updated, I've introduced the state for those and the setState to update them because it is the recommended way to to these operations. WDYT?

--- src/components/Modal/Modal.context.tsx
+++ src/components/Modal/Modal.context.tsx
@@ -1,21 +1,21 @@
-import React, { useState, createContext, Children, cloneElement } from "react";
+import React, { ReactNode, useState, createContext, Children, cloneElement } from "react";
 import { timeout } from "./constants";
 
 // Provides a context for custom modals which need a context.
 // Simply assign this object to the exported component, and it will
 // be a Modal.context instance.
-export default () => {
-  const context = createContext();
+export default (): ReactNode => {
+  const context = createContext({});
 
   /**
    * The context provider.
    */
-  const Provider = ({ children, ...rest }) => {
+  const Provider = ({ children, ...rest }: any) => {
     const [isOpen, toggleModal] = useState(false);
 
     // Add a wrapper function to call the callback whenever the modal
     // is totally closed or opened.
-    const toggleModalWrapper = (val, cb) => {
+    const toggleModalWrapper = (val: boolean, cb: any) => {
       toggleModal(val);
       cb && setTimeout(cb, timeout);
     };
