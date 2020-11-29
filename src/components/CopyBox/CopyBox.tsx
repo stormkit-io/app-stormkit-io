68b5eae7d924ec93fb3663d39ee515d548798fdb
Ivan Lori <ivan.lori@protonmail.com>
2020-11-29 10:05:47 +0100

    Refactor: migration of copybox to TS

---
+++ src/components/CopyBox/CopyBox.tsx
@@ -0,0 +1,33 @@
+import React, { FC, ReactElement } from "react";
+import Form from "~/components/Form";
+import Button from "~/components/Button";
+
+type Props = {
+  value: string;
+}
+
+const CopyBox: FC<Props> = ({ value }: Props): ReactElement => (
+  <>
+    <Form.Input
+      multiline
+      id="copy-token"
+      defaultValue={value}
+      className="flex-auto"
+      inputProps={{
+        "aria-label": "Copy token"
+      }}
+    />
+    <Button
+      type="button"
+      onClick={() => {
+        (document.querySelector("#copy-token") as HTMLInputElement).focus();
+        (document.querySelector("#copy-token") as HTMLInputElement).select();
+        document.execCommand("copy");
+      }}
+    >
+      <span className="far fa-copy" />
+    </Button>
+  </>
+);
+
+export default CopyBox;
