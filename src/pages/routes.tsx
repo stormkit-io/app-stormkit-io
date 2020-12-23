a843587e6fb09467775e90d9765ec13a29f7af1c
Savas Vedova <savas@stormkit.io>
2020-12-23 11:35:57 +0300

    Feat: redirect paths starting with app to apps for compatibility

---
+++ src/pages/routes.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Redirect, useLocation } from "react-router-dom";
+import Async from "~/components/Async";
+import Spinner from "~/components/Spinner";
+
+Async.configure({ Loader: Spinner, props: { pageCenter: true } });
+
+export default [
+  {
+    path: "/",
+    exact: true,
+    component: Async(() => import("~/pages/apps")),
+  },
+  {
+    path: "/auth",
+    component: Async(() => import("~/pages/auth")),
+  },
+  {
+    path: "/logout",
+    component: Async(() => import("~/pages/logout")),
+  },
+  {
+    path: "/apps/new",
+    exact: true,
+    component: Async(() => import("~/pages/apps/new")),
+  },
+  {
+    path: "/apps/new/:provider",
+    exact: true,
+    component: Async(() => import("~/pages/apps/new/[provider]")),
+  },
+  {
+    path: "/app/:id",
+    component: (): React.ReactElement => {
+      const { pathname } = useLocation();
+      return <Redirect to={pathname.replace("/app", "/apps")} />;
+    },
+  },
+  {
+    path: "/apps/:id",
+    component: Async(() => import("~/pages/apps/App.context")),
+  },
+  {
+    path: "/user",
+    component: Async(() => import("~/pages/user")),
+  },
+];
