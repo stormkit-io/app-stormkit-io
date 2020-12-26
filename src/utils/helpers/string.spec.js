4e4cd1e08395fac7859767240d5c03852ea0a2dd
Savas Vedova <savas@stormkit.io>
2020-12-27 00:50:18 +0300

    Feat: add utils to parse boolean

--- src/utils/helpers/string.spec.js
+++ src/utils/helpers/string.spec.js
@@ -1,8 +1,20 @@
-import { capitalize } from "./string";
+import { capitalize, parseBoolean, booleanToString } from "./string";
 
 describe("utils/helpers/string", () => {
   test("capitalize", () => {
     expect(capitalize("my string")).toBe("My string");
     expect(capitalize()).toBe("");
+  });
+
+  test("parseBoolean", () => {
+    expect(parseBoolean("true")).toBe(true);
+    expect(parseBoolean("false")).toBe(false);
+    expect(parseBoolean("")).toBeUndefined();
+  });
+
+  test("booleanToString", () => {
+    expect(booleanToString(true)).toBe("true");
+    expect(booleanToString(false)).toBe("false");
+    expect(booleanToString()).toBe("");
   });
 });
