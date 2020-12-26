4e860eb31fa9626cb8ef814133e1787430215eed
Savas Vedova <savas@stormkit.io>
2020-12-27 00:50:18 +0300

    Feat: add utils to parse boolean

--- src/utils/helpers/string.ts
+++ src/utils/helpers/string.ts
@@ -8,3 +8,24 @@ export const capitalize = (value?: string): string => {
 
   return value.charAt(0).toUpperCase() + value.slice(1);
 };
+
+/**
+ * Takes a boolean value and casts it into a string. For instance,
+ * if the value is `true`, this will return a string "true". When
+ * value is undefined, it will return an empty string.
+ */
+export const booleanToString = (value?: boolean): string => {
+  if (typeof value === "undefined") {
+    return "";
+  }
+
+  return value ? "true" : "false";
+};
+
+/**
+ * Takes a string value and parses it into a string. If the
+ * string is any value other than "true" or "false", it will return undefined.
+ */
+export const parseBoolean = (value?: string): boolean | undefined => {
+  return value === "true" ? true : value === "false" ? false : undefined;
+};
