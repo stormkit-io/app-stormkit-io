b47964d8cd3f9c5a06dc71d4f799f5d68a906711
Ivan Lori <ivan.lori@protonmail.com>
2020-12-30 11:31:55 +0100

    Refactor: ts migration of center layout

---
+++ src/layouts/CenterLayout.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement, ReactNode, FC } from "react";
+
+type Props = {
+  children: ReactNode;
+}
+
+const DefaultLayout: FC<Props> = ({ children }: Props): ReactElement => (
+  <main className="flex flex-col max-w-screen-xl min-h-screen m-auto items-center justify-center">
+    {children}
+  </main>
+);
+
+export default DefaultLayout;
