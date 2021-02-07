f7ffcd41ca7b396770997d9f4f6bf73b1e158439
Kev <kevslashnull@gmail.com>
2021-02-07 23:22:04 +0100

    Run prettier on all files

--- src/layouts/CenterLayout.tsx
+++ src/layouts/CenterLayout.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, ReactNode, FC } from "react";
 
 type Props = {
   children: ReactNode;
-}
+};
 
 const DefaultLayout: FC<Props> = ({ children }: Props): ReactElement => (
   <main className="flex flex-col max-w-screen-xl min-h-screen m-auto items-center justify-center">
