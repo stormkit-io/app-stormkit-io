cad9f80e9330c428966d0633cecfc28fe5f2a3ee
can eldem <eldemcan@gmail.com>
2021-04-19 22:00:00 +0100

    Use sentry using dynamic import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 import { BrowserRouter as Router } from "react-router-dom";
 import "~/assets/styles/tailwind.css";
 import "~/assets/styles/global.css";
@@ -10,11 +8,15 @@ import RootContext from "~/pages/Root.context";
 import Root from "~/pages/Root";
 
 if (process.env.SENTRY) {
-  Sentry.init({
-    dsn: process.env.SENTRY,
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0
-  });
+  Promise.all([import("@sentry/react"), import("@sentry/tracing")]).then(
+    ([Sentry, { Integrations }]) => {
+      Sentry.init({
+        dsn: process.env.SENTRY,
+        integrations: [new Integrations.BrowserTracing()],
+        tracesSampleRate: 1.0
+      });
+    }
+  );
 }
 
 // for hot reloading
