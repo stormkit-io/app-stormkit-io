d7e4ba2f886ed7f32ceead62a09350126fb1d6d7
Savas Vedova <savas@stormkit.io>
2022-01-13 13:40:36 +0300

    chore: add tests for outbound webhooks

--- src/pages/apps/[id]/settings/_components/FormOutboundWebhooks.tsx
+++ src/pages/apps/[id]/settings/_components/FormOutboundWebhooks.tsx
@@ -1,25 +1,38 @@
 import React, { useState } from "react";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import { useLocation, useHistory } from "react-router-dom";
-import { connect } from "~/utils/context";
+import { useHistory } from "react-router-dom";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
 import Form from "~/components/Form";
 import InfoBox from "~/components/InfoBox";
 import Button from "~/components/Button";
-import type { ModalContextProps } from "~/components/Modal";
+import Modal from "~/components/Modal";
+import DotDotDot from "~/components/DotDotDot";
 import { RootContextProps } from "~/pages/Root.context";
+import { truncate } from "~/utils/helpers/string";
 import FormNewOutboundWebhookModal from "./FormNewOutboundWebhookModal";
-import { useFetchOutboundWebhooks } from "../_actions/outbound_webhook_actions";
+import {
+  useFetchOutboundWebhooks,
+  sendSampleRequest,
+  deleteOutboundWebhook,
+} from "../_actions/outbound_webhook_actions";
+import type { SendSampleRequestResponse } from "../_actions/outbound_webhook_actions";
+import Spinner from "~/components/Spinner";
 
 interface Props extends Pick<RootContextProps, "api"> {
   app: App;
 }
 
-const FormOutboundWebhooks: React.FC<Props & ModalContextProps> = ({
+const FormOutboundWebhooks: React.FC<Props> = ({
   api,
   app,
-  toggleModal,
 }): React.ReactElement => {
-  const [loading, setLoading] = useState(false);
+  const history = useHistory();
+  const [loadingSample, setLoadingSample] = useState(false);
+  const [sampleData, setSampleData] = useState<SendSampleRequestResponse>();
+  const [isModalOpen, toggleModal] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const hooks = useFetchOutboundWebhooks({
@@ -30,50 +43,124 @@ const FormOutboundWebhooks: React.FC<Props & ModalContextProps> = ({
   });
 
   return (
-    <Form>
-      <Form.Section
-        label={
-          <div className="flex items-center" id="outbound-webhooks">
-            Outbound Webhooks
-          </div>
-        }
-        marginBottom="mb-4"
-      >
-        {hooks.length ? (
-          hooks.map(hook => hook.id)
-        ) : (
-          <InfoBox>
-            You don't have any webhook installed. Click Add new webhook to
-            create a new one.
-          </InfoBox>
-        )}
-        {error && (
-          <InfoBox type={InfoBox.ERROR} toaster dismissable>
-            {error}
-          </InfoBox>
-        )}
-      </Form.Section>
-      <div className="flex justify-end">
-        <Button
-          primary
-          type="submit"
-          loading={loading}
-          onClick={() => {
-            toggleModal(true);
+    <>
+      <Form>
+        <Form.Section
+          label={
+            <div className="flex items-center" id="outbound-webhooks">
+              Outbound Webhooks
+            </div>
+          }
+          marginBottom="mb-4"
+        >
+          {hooks.length ? (
+            <Table>
+              <TableBody>
+                {hooks.map(hook => (
+                  <TableRow key={hook.id}>
+                    <TableCell>{truncate(hook.requestUrl, 50)}</TableCell>
+                    <TableCell align="right">
+                      <DotDotDot
+                        aria-label={`Outbound webhook ${hook.id} menu`}
+                      >
+                        <DotDotDot.Item
+                          icon="fas fa-trash-alt text-red-50 mr-2"
+                          onClick={close => {
+                            setLoading(true);
+                            deleteOutboundWebhook({
+                              app,
+                              api,
+                              whId: hook.id,
+                            }).then(() => {
+                              history.replace({
+                                state: {
+                                  outboundWebhooksRefresh: Date.now(),
+                                },
+                              });
+
+                              close();
+                            });
+                          }}
+                        >
+                          Delete
+                        </DotDotDot.Item>
+                        <DotDotDot.Item
+                          onClick={close => {
+                            setLoadingSample(true);
+
+                            sendSampleRequest({ app, api, whId: hook.id })
+                              .then(res => {
+                                setLoadingSample(false);
+                                setSampleData(res);
+                              })
+                              .finally(() => {
+                                close();
+                              });
+                          }}
+                        >
+                          Trigger sample{" "}
+                          {loadingSample && (
+                            <Spinner
+                              className="ml-2"
+                              primary
+                              width={4}
+                              height={4}
+                            />
+                          )}
+                        </DotDotDot.Item>
+                      </DotDotDot>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          ) : (
+            <InfoBox>
+              You don't have any webhook installed. Click Add new webhook to
+              create a new one.
+            </InfoBox>
+          )}
+          {error && (
+            <InfoBox type={InfoBox.ERROR} toaster dismissable>
+              {error}
+            </InfoBox>
+          )}
+        </Form.Section>
+        <div className="flex justify-end">
+          <Button
+            primary
+            type="submit"
+            aria-label="Add new webhook"
+            loading={loading}
+            onClick={() => {
+              toggleModal(true);
+            }}
+          >
+            Add new webhook
+          </Button>
+        </div>
+        <Modal
+          className="max-w-screen-sm"
+          isOpen={Boolean(sampleData)}
+          onClose={() => {
+            setSampleData(undefined);
           }}
         >
-          Add new webhook
-        </Button>
-      </div>
-      <FormNewOutboundWebhookModal app={app} api={api} />
-    </Form>
+          <h3 className="mb-8 text-xl font-bold">
+            Sample result: {sampleData?.result.status}
+          </h3>
+          <code>{sampleData?.result.body || "Empty response"}</code>
+        </Modal>
+      </Form>
+
+      <FormNewOutboundWebhookModal
+        app={app}
+        api={api}
+        isOpen={isModalOpen}
+        toggleModal={toggleModal}
+      />
+    </>
   );
 };
 
-export default connect<Props, ModalContextProps>(FormOutboundWebhooks, [
-  {
-    Context: FormNewOutboundWebhookModal,
-    props: ["toggleModal"],
-    wrap: true,
-  },
-]);
+export default FormOutboundWebhooks;
