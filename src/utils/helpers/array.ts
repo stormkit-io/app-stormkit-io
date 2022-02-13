9f20e2615e2655e05adb91c8261740af0408bfc2
Savas Vedova <savas@stormkit.io>
2022-02-13 19:13:20 +0300

    chore: update function signature

--- src/utils/helpers/array.ts
+++ src/utils/helpers/array.ts
@@ -1,3 +1,3 @@
-export function toArray<T>(v: T): Array<T> {
+export function toArray<T>(v: T | Array<T>): Array<T> {
   return (Array.isArray(v) ? v : [v]).filter(i => typeof i !== "undefined");
 }
