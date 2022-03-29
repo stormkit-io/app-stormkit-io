954e700050387268a1de9bc7e014a73fa7d97013
Savas Vedova <savas@stormkit.io>
2022-03-30 00:22:17 +0200

    feat: allow specifying padding top for section

--- src/components/Form/components/Section.tsx
+++ src/components/Form/components/Section.tsx
@@ -1,22 +1,26 @@
 import React, { ReactNode, FC, ReactElement } from "react";
 import cn from "classnames";
 
-type Props = {
+interface Props {
+  paddingTop?: string; // The tailwind class for the marginBottom
   marginBottom?: string; // The tailwind class for the marginBottom
   className?: string;
   children: ReactNode;
   label: ReactNode;
-};
+}
 
 const Section: FC<Props> = ({
   children,
   className,
   label,
+  paddingTop = "pt-4",
   marginBottom = "mb-8",
 }: Props): ReactElement => {
   return (
     <div className={cn("flex", marginBottom, className)}>
-      <div className="flex-auto pt-4 min-w-48 max-w-48 font-bold">{label}</div>
+      <div className={cn("flex-auto min-w-48 max-w-48 font-bold", paddingTop)}>
+        {label}
+      </div>
       <div className="flex-auto">{children}</div>
     </div>
   );
