b2973b4cfcb2b52bec003a91bf618bf99f2bfc38
Can Eldem <eldemcan@users.noreply.github.com>
2022-06-24 15:09:20 +0100

    feat: add announcement module for users to see what is new
    feat: make remote config inaccessible from ui

---
+++ src/layouts/_components/News.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import OutsideClick from "~/components/OutsideClick";
+import { Popper } from "@material-ui/core";
+
+const News: React.FC = (): React.ReactElement => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  function handleClick(event: any) {
+    setAnchorEl(anchorEl ? null : event.currentTarget);
+  }
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popper" : undefined;
+
+  return (
+    <OutsideClick handler={() => setAnchorEl(null)}>
+      <div>
+        <button aria-describedby={id} className="p-2" onClick={handleClick}>
+          <span className="fa-solid fa-bullhorn fa-lg hover:text-white" />
+        </button>
+        <Popper id={id} open={open} anchorEl={anchorEl} placement="bottom-end">
+          <div className="bg-white">
+            <iframe
+              className="h-96"
+              style={{ width: "38rem" }}
+              src="https://www.stormkit.io/blog/whats-new"
+            />
+          </div>
+        </Popper>
+      </div>
+    </OutsideClick>
+  );
+};
+
+export default News;
