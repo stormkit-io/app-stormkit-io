5b6bfe05b971988112a0b1afde41434b36d1c9ac
Can Eldem <eldemcan@users.noreply.github.com>
2022-07-28 23:02:28 +0100

    feat: disable/enable canary by clicking label
    instead of enable/disable canary env from console
    we can now enable/disable by clicking Canary label

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import cn from "classnames";
 import logoText from "~/assets/logos/stormkit-logo-text-h-white.svg";
 import logoIcon from "~/assets/logos/stormkit-logo-circle.svg";
-
 interface Props {
   iconOnly?: boolean;
   iconSize?: number;
@@ -15,21 +14,43 @@ const Logo: React.FC<Props> = ({
   iconSize = 16,
   ...rest
 }: Props): React.ReactElement => {
+
+  const isCanary = () =>
+    window.document.cookie.indexOf("sk_canary=true") > -1 ? true : false;
+  const enableCanary = () => {
+    window.document.cookie = "sk_canary=true";
+  };
+  const disableCanary = () => {
+    window.document.cookie = "sk_canary=false";
+  };
+
+  const color = isCanary() ? "text-white" : "text-gray-800"
+
   return (
-    <span className={cn("inline-flex items-center", className)} {...rest}>
-      <img
-        src={iconOnly ? logoIcon : logoText}
-        alt="Stormkit Logo"
-        className={cn(`inline-block w-${iconSize}`, {
-          "w-28": !iconOnly,
-          "w-8": iconOnly,
-        })}
-      />
-      {!iconOnly && (
-        <span className="font-bold text-lg text-secondary hover:text-white">
-          {document.cookie.indexOf("sk_canary=true") > -1 ? "canary" : ""}
-        </span>
-      )}
+    <span
+      className={cn("flex flex-direction row items-center", className)}
+      {...rest}
+    >
+      <div className="flex">
+        <img
+          src={iconOnly ? logoIcon : logoText}
+          alt="Stormkit Logo"
+          className={cn(`inline-block w-${iconSize}`, {
+            "w-28": !iconOnly,
+            "w-8": iconOnly,
+          })}
+        />
+        {!iconOnly && (
+          <div>
+            <div
+              style={{ padding: "10px" }}
+              onClick={() => (isCanary() ? disableCanary() : enableCanary() )}
+            >
+              <p className={color} >Canary</p>
+            </div>
+          </div>
+        )}
+      </div>
     </span>
   );
 };
