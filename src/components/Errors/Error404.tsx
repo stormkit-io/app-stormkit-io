99db2db563958f970750792f2a168305d28b9bca
Savas Vedova <savas@stormkit.io>
2022-09-10 21:26:07 +0200

    chore: minor improvements

--- src/components/Errors/Error404.tsx
+++ src/components/Errors/Error404.tsx
@@ -3,21 +3,27 @@ import Link from "~/components/Link";
 import Logo from "~/components/Logo";
 
 interface Props {
+  withLogo?: boolean;
   children?: React.ReactNode;
 }
 
-const Error404: React.FC<Props> = ({ children }): React.ReactElement => {
+const Error404: React.FC<Props> = ({
+  children,
+  withLogo = true,
+}): React.ReactElement => {
   return (
     <div className="flex flex-col m-auto max-w-lg items-center h-full justify-center">
       <h1 className="text-pink-50 text-9xl mb-12 font-bold">4 oh 4</h1>
       <div className="text-white text-3xl text-center leading-normal">
         {children || "There is nothing under this link."}
       </div>
-      <div className="mt-32">
-        <Link to="/">
-          <Logo iconOnly />
-        </Link>
-      </div>
+      {withLogo && (
+        <div className="mt-32">
+          <Link to="/">
+            <Logo iconOnly />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
