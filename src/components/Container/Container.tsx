f5caa3913e719464fa2bba87f15ee037d1ade0ac
Savas Vedova <savas@stormkit.io>
2022-09-19 22:12:18 +0300

    chore: add new size

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,7 +3,7 @@ import cn from "classnames";
 
 interface Props {
   bg?: "bg-blue-50";
-  maxWidth?: "w-max-w-2xl";
+  maxWidth?: "max-w-2xl" | "max-w-none";
   margin?: "m-auto";
   title?: React.ReactNode;
   actions?: React.ReactNode;
@@ -24,12 +24,14 @@ const Container: React.FC<Props> = ({
     <div
       className={cn(["w-full text-gray-80", margin, maxWidth, bg, className])}
     >
-      <div className="flex p-4">
-        {title && <h3 className="flex items-center font-bold">{title}</h3>}
-        {actions && (
-          <div className="flex flex-1 justify-end text-sm">{actions}</div>
-        )}
-      </div>
+      {(title || actions) && (
+        <div className="flex p-4">
+          {title && <h3 className="flex items-center font-bold">{title}</h3>}
+          {actions && (
+            <div className="flex flex-1 justify-end text-sm">{actions}</div>
+          )}
+        </div>
+      )}
       {children}
     </div>
   );
