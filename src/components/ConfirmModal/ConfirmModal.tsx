edb93c9cddfb008698ceac4f8cb912f6643ce41c
Savas Vedova <savas@stormkit.io>
2022-09-21 20:37:20 +0300

    chore: use modal v2

--- src/components/ConfirmModal/ConfirmModal.tsx
+++ src/components/ConfirmModal/ConfirmModal.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import Modal from "~/components/Modal";
-import Form from "~/components/Form";
-import Button from "~/components/Button";
+import Modal from "~/components/ModalV2";
+import Form from "~/components/FormV2";
+import Container from "~/components/Container";
+import Button from "~/components/ButtonV2";
 import InfoBox from "~/components/InfoBox";
 
 type ConfirmModalCallback = ({
@@ -46,52 +47,54 @@ const ConfirmModal: React.FC<Props> = ({
   };
 
   return (
-    <Modal isOpen onClose={onCancel} className="sm:max-w-screen-sm">
-      <h2 className="font-bold text-2xl text-center mb-16">Confirm action</h2>
-      <div className="text-sm text-center">
-        <p className="mb-2">{children}</p>
+    <Modal open onClose={onCancel} className="sm:max-w-screen-sm">
+      <Container className="p-4">
+        <h2 className="font-bold text-2xl text-center mb-12">Confirm action</h2>
+        <div className="text-sm text-center">
+          <p className="mb-2">
+            {children}
+            {!typeConfirmationText && " Are you sure you want to continue?"}
+          </p>
 
-        {typeConfirmationText ? (
-          <>
-            <p>
-              Type <b>{typeConfirmationText}</b> in order to proceed.
-            </p>
-            <Form.Input
-              className="mt-4"
-              placeholder={typeConfirmationText}
-              onChange={verifyConfirmationInput}
-              fullWidth
-              autoFocus
-            />
-          </>
-        ) : (
-          <p>Are you sure you want to continue?</p>
+          {typeConfirmationText && (
+            <>
+              <p>
+                Type <b>{typeConfirmationText}</b> in order to proceed.
+              </p>
+              <Form.Input
+                className="mt-4"
+                placeholder={typeConfirmationText}
+                onChange={verifyConfirmationInput}
+                fullWidth
+                autoFocus
+              />
+            </>
+          )}
+        </div>
+        {error && (
+          <InfoBox className="mt-8" type={InfoBox.ERROR}>
+            {error}
+          </InfoBox>
         )}
-      </div>
-      {error && (
-        <InfoBox className="mt-8" type={InfoBox.ERROR}>
-          {error}
-        </InfoBox>
-      )}
-      <div className="mt-16 text-sm flex justify-center">
-        <Button
-          secondary
-          type="button"
-          className="py-2 mr-4"
-          onClick={onCancel}
-        >
-          Cancel
-        </Button>
-        <Button
-          primary
-          className="py-2"
-          loading={loading}
-          disabled={!confirmButtonEnabled}
-          onClick={handleSuccess}
-        >
-          {confirmText || "Yes, continue"}
-        </Button>
-      </div>
+        <div className="mt-12 text-sm flex justify-center">
+          <Button
+            category="cancel"
+            type="button"
+            className="mr-4"
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+          <Button
+            category="action"
+            loading={loading}
+            disabled={!confirmButtonEnabled}
+            onClick={handleSuccess}
+          >
+            {confirmText || "Yes, continue"}
+          </Button>
+        </div>
+      </Container>
     </Modal>
   );
 };
