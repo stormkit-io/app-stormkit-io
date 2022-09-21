20c90f9d67da31a0ef0390dff9e06277a7f9a167
Savas Vedova <savas@stormkit.io>
2022-09-21 18:57:16 +0300

    chore: move loading icon inside

--- src/pages/apps/[id]/environments/[env-id]/_components/DomainStatus.tsx
+++ src/pages/apps/[id]/environments/[env-id]/_components/DomainStatus.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 import cn from "classnames";
+import Spinner from "~/components/Spinner";
 
 interface StatusProps {
   status?: number;
+  loading?: boolean;
 }
 
 const DomainStatus: React.FC<StatusProps> = ({
   status,
+  loading,
 }): React.ReactElement => {
   return (
     <div className="flex items-center">
-      <span
-        className={cn(
-          {
-            "text-green-50": status === 200,
-            "text-red-50": status && status !== 200,
-          },
-          "flex",
-          "items-center",
-          "align-baseline"
-        )}
-      >
-        <span className={cn("fa-solid fa-globe", { "mr-2": status })} />
-        {status}
-      </span>
+      {loading && <Spinner width={4} height={4} />}
+      {!loading && (
+        <span
+          className={cn(
+            {
+              "text-green-50": status === 200,
+              "text-red-50": status && status !== 200,
+            },
+            "flex",
+            "items-center",
+            "align-baseline"
+          )}
+        >
+          <span className={cn("fa-solid fa-globe", { "mr-2": status })} />
+          <span className="font-light">{status}</span>
+        </span>
+      )}
     </div>
   );
 };
