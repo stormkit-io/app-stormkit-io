cb2460937249c347a4b69c897d8971a5797e2cc7
Savas Vedova <savas@stormkit.io>
2022-10-08 22:36:15 +0200

    chore: update styling

--- src/components/Errors/Error404.tsx
+++ src/components/Errors/Error404.tsx
@@ -13,7 +13,7 @@ const Error404: React.FC<Props> = ({
 }): React.ReactElement => {
   return (
     <div className="flex flex-col m-auto max-w-lg items-center h-full justify-center">
-      <h1 className="text-pink-50 text-9xl mb-12 font-bold">4 oh 4</h1>
+      <h1 className="text-pink-50 text-9xl font-bold">4 oh 4</h1>
       <div className="text-white text-3xl text-center leading-normal">
         {children || "There is nothing under this link."}
       </div>
