f2cd443f0f365c02cfcd6a43bff82dad1bfb8e65
Savas Vedova <savas@stormkit.io>
2022-10-08 21:03:51 +0200

    chore: component to display environment status

---
+++ src/pages/apps/[id]/environments/_components/EnvironmentStatus.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import cn from "classnames";
+import Link from "~/components/Link";
+import { useFetchStatus } from "../actions";
+
+interface Props {
+  env: Environment;
+  app: App;
+}
+
+const EnvironmentStatus: React.FC<Props> = ({ env, app }) => {
+  const endpoint = env.customStorage?.externalUrl || env?.preview || "";
+  const { status } = useFetchStatus({
+    environment: env,
+    app,
+    domain: endpoint,
+  });
+
+  if (env.published) {
+    return (
+      <>
+        <div className="flex">
+          <label className="flex w-20 text-gray-50">Endpoint</label>
+          <span>
+            <span className="fa fa-external-link w-6 text-gray-50"></span>
+            <Link to={endpoint}>{endpoint.replace(/https?:\/\//, "")}</Link>
+          </span>
+        </div>
+        <div className="flex mt-3">
+          <label className="flex w-20 text-gray-50">Published</label>
+          <span>
+            <span
+              className="fa fa-ship w-6 text-gray-50"
+              style={{ marginLeft: "-1px" }}
+            ></span>
+            {env.published.map(p => (
+              <Link
+                key={p.deploymentId}
+                to={`/apps/${env.appId}/environments/${env.id}/deployments/${p.deploymentId}`}
+              >
+                {p.deploymentId}
+              </Link>
+            ))}
+          </span>
+        </div>
+        <div className="flex mt-3">
+          <label className="flex w-20 text-gray-50">Status</label>
+          <span
+            className={cn({
+              "text-green-50": status === 200 || status === 304,
+            })}
+          >
+            <span
+              className={cn("fa fa-globe w-6", {
+                "text-gray-50": status !== 200 && status !== 304,
+              })}
+            ></span>
+            {status}
+          </span>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex items-center">
+        <label className="flex w-20 text-gray-50">Status</label>
+        <span
+          className={cn("w-2 h-2 inline-block mr-4", {
+            "bg-green-50": env.lastDeploy?.exit === 0,
+            "bg-red-50": env.lastDeploy?.exit !== 0,
+            "bg-yellow-50": !env.lastDeploy,
+          })}
+        />
+        {env.lastDeploy?.createdAt ? (
+          env.lastDeploy?.exit === 0 ? (
+            <span>
+              Deployed successfully.{" "}
+              <Link
+                to={`/apps/${app.id}/environments/${env.id}/deployments/${env.lastDeploy.id}`}
+              >
+                Go to deployment <span className="fa fa-chevron-right" />
+              </Link>
+            </span>
+          ) : (
+            "Deployment failed."
+          )
+        ) : (
+          "Not yet deployed. Deploy now and publish it to serve your application."
+        )}
+      </div>
+    </>
+  );
+};
+
+export default EnvironmentStatus;
