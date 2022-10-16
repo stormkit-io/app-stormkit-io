c3adde4b7f0939f2aacd420c1a0daab098d7d873
Savas Vedova <savas@stormkit.io>
2022-10-16 16:07:54 +0200

    refactor: upgrade modal to v2

--- src/pages/apps/_components/WelcomeModal.tsx
+++ src/pages/apps/_components/WelcomeModal.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { LocalStorage } from "~/utils/storage";
-import Modal from "~/components/Modal";
-import Button from "~/components/Button";
+import Modal from "~/components/ModalV2";
+import Button from "~/components/ButtonV2";
+import Container from "~/components/Container";
 
 interface Props {
   isOpen: boolean;
@@ -20,49 +21,61 @@ const Welcome: React.FC<Props> = ({
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={close} className="max-w-screen-sm">
-      <h1 className="font-bold text-xl text-center mb-8 mt-12 lg:mt-0">
-        Welcome to Stormkit 🎉
-      </h1>
-      <Button
-        as="div"
-        href="https://discord.gg/6yQWhyY"
-        className="flex justify-between shadow w-full bg-gray-90 hover:bg-gray-70 p-8 mb-8"
-      >
-        <div className="text-3xl mr-8">
-          <i className="fa-brands fa-discord" style={{ color: "#5865F2" }}></i>
-        </div>
-        <div className="flex-grow">
-          <h2 className="text-black mb-2 font-bold">
-            Join our Discord community
-          </h2>
-          <p className="text-sm text-black">
-            Ask questions and join discussions.
-          </p>
-        </div>
-        <span className="fas fa-chevron-right text-base ml" />
-      </Button>
-      <Button
-        as="div"
-        href="https://twitter.com/stormkitio"
-        className="flex justify-between shadow w-full bg-gray-90 hover:bg-gray-70 p-8"
-      >
-        <div className="text-3xl mr-8">
-          <i className="fa-brands fa-twitter" style={{ color: "#00acee" }}></i>
-        </div>
-        <div className="flex-grow">
-          <h2 className="text-black mb-2 font-bold">Follow us on Twitter</h2>
-          <p className="text-sm text-black">
-            Stay tuned about latest developments on Stormkit.
-          </p>
-        </div>
-        <span className="fas fa-chevron-right text-base ml" />
-      </Button>
-      <div className="lg:hidden mt-8 flex justify-center w-full">
-        <Button secondary onClick={close}>
-          Close
+    <Modal
+      open={isOpen}
+      onClose={close}
+      className="max-w-screen-sm h-full md:h-auto"
+    >
+      <Container className="p-4 md:p-8 justify-center h-full md:h-auto flex md:block flex-col md:flex-none">
+        <h1 className="font-bold text-xl md:text-3xl text-center mb-8">
+          Welcome to Stormkit 🎉
+        </h1>
+        <Button
+          styled={false}
+          category="button"
+          type="button"
+          href="https://discord.gg/6yQWhyY"
+          className="flex justify-between items-center shadow w-full p-4 md:p-8 bg-blue-10 hover:bg-black text-gray-80 mb-8"
+        >
+          <span className="text-3xl mr-8">
+            <i
+              className="fa-brands fa-discord"
+              style={{ color: "#5865F2" }}
+            ></i>
+          </span>
+          <span className="flex flex-col flex-grow">
+            <span className="font-bold">Join our Discord community</span>
+            <span className="text-sm">Ask questions and join discussions.</span>
+          </span>
+          <span className="fas fa-chevron-right text-base ml" />
         </Button>
-      </div>
+        <Button
+          styled={false}
+          type="button"
+          category="button"
+          href="https://twitter.com/stormkitio"
+          className="flex justify-between items-center shadow w-full p-4 md:p-8 bg-blue-10 hover:bg-black text-gray-80"
+        >
+          <span className="text-3xl mr-8">
+            <i
+              className="fa-brands fa-twitter"
+              style={{ color: "#00acee" }}
+            ></i>
+          </span>
+          <span className="flex flex-col flex-grow">
+            <span className="font-bold">Follow us on Twitter</span>
+            <span className="text-sm">
+              Stay tuned about latest developments on Stormkit.
+            </span>
+          </span>
+          <span className="fas fa-chevron-right text-base ml" />
+        </Button>
+        <div className="mt-8 flex justify-end w-full">
+          <Button type="button" category="button" onClick={close}>
+            Close
+          </Button>
+        </div>
+      </Container>
     </Modal>
   );
 };
