2c879451f08a7fd911b3bffb1743246f04893660
Savas Vedova <savas@stormkit.io>
2022-10-18 10:15:13 +0200

    refactor: feature flags v2

--- src/pages/apps/[id]/environments/[env-id]/feature-flags/actions.ts
+++ src/pages/apps/[id]/environments/[env-id]/feature-flags/actions.ts
@@ -10,8 +10,6 @@ interface FetchFeatureFlagsReturnValue {
   flags: FeatureFlag[];
   error: string | null;
   loading: boolean;
-  setError: SetError;
-  setLoading: SetLoading;
   setReload: (val: number) => void;
 }
 
@@ -51,140 +49,53 @@ export const useFetchFeatureFlags = ({
     };
   }, [reload]);
 
-  return { flags, loading, error, setError, setLoading, setReload };
+  return { flags, loading, error, setReload };
 };
 
 interface DeleteFeatureFlagArgs {
   app: App;
   environment: Environment;
   flagName: string;
-  setReload: (val: number) => void;
-  setLoading: SetLoading;
-  setError: SetError;
-  closeModal: () => void;
 }
 
 export function deleteFeatureFlag({
   app,
   environment,
-  setReload,
-  setLoading,
-  setError,
-  closeModal,
   flagName,
-}: DeleteFeatureFlagArgs): void {
-  setLoading(true);
-  api
-    .delete(`/apps/flags`, {
-      appId: app.id,
-      envId: environment.id,
-      flagName: flagName,
-    })
-    .then(() => {
-      setReload(Date.now());
-      closeModal();
-    })
-    .catch(() => {
-      setError(
-        "Something went wrong while deleting the feature flag. Please try again, if the problem persists contact us from Discord or email."
-      );
-    })
-    .finally(() => {
-      setLoading(false);
-    });
-}
-
-interface UpdateFeatureFlagStatus {
-  app: App;
-  environment: Environment;
-  flag: FeatureFlag;
-  setError: SetError;
-  setLoading: SetLoading;
-}
-
-export function updateFeatureFlagStatus({
-  app,
-  environment,
-  setError,
-  setLoading,
-  flag,
-}: UpdateFeatureFlagStatus) {
-  if (flag.flagName === "" || flag.flagValue === undefined) {
-    return setError("Wrong params");
-  }
-
-  return api
-    .post(`/apps/flags`, {
-      appId: app.id,
-      flagName: flag.flagName,
-      flagValue: flag.flagValue,
-      envId: environment.id,
-    })
-    .catch(() => {
-      const error =
-        "Something went wrong on our side. Please try again later or reach us if the problem persists.";
-
-      if (typeof setError === "function") {
-        setError(error);
-      } else {
-        throw new Error(error);
-      }
-    })
-    .finally(() => {
-      setLoading(false);
-    });
+}: DeleteFeatureFlagArgs): Promise<void> {
+  return api.delete(`/apps/flags`, {
+    appId: app.id,
+    envId: environment.id,
+    flagName: flagName,
+  });
 }
 
 interface UpsertFeatureFlagArgs {
   app: App;
   environment: Environment;
-  setError: SetError;
-  setLoading: SetLoading;
-  closeModal: () => void;
-  setReload: (val: number) => void;
+  values: FormValues;
 }
 
-interface FormValues {
-  name: string;
-  status: "true" | "false";
+export interface FormValues {
+  flagName: string;
+  flagValue: boolean | "on" | "off";
 }
 
-export const upsertFeatureFlag =
-  ({
-    app,
-    environment,
-    setError,
-    setLoading,
-    closeModal,
-    setReload,
-  }: UpsertFeatureFlagArgs) =>
-  (values: FormValues) => {
-    const name = values.name.trim();
-    const status = values.status.trim() === "true" ? true : false;
+export const upsertFeatureFlag = ({
+  app,
+  environment,
+  values,
+}: UpsertFeatureFlagArgs): Promise<void> => {
+  const name = values.flagName.trim();
 
-    if (name === "") {
-      return setError("Name cannot be empty.");
-    }
+  if (name === "") {
+    return Promise.reject("Flag name cannot be empty.");
+  }
 
-    setLoading(true);
-
-    return api
-      .post(`/apps/flags`, {
-        appId: app.id,
-        flagName: name,
-        flagValue: status,
-        envId: environment.id,
-      })
-      .then(() => {
-        setReload(Date.now());
-        closeModal();
-      })
-      .catch(() => {
-        setError(
-          "Something went wrong on our side. Please try again later or reach us if the problem persists."
-        );
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  return api.post(`/apps/flags`, {
+    appId: app.id,
+    flagName: name,
+    flagValue: values.flagValue === true || values.flagValue === "on",
+    envId: environment.id,
+  });
+};
--- src/pages/apps/[id]/environments/[env-id]/feature-flags/index.ts
+++ src/pages/apps/[id]/environments/[env-id]/feature-flags/index.ts
@@ -1,1 +1,1 @@
-export { default } from "./FeatureFlag";
+export { default } from "./FeatureFlags";
