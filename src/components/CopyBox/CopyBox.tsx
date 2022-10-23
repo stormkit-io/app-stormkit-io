eeb2c6408fd7cfa3c9b1536344daf58546df60a6
Savas Vedova <savas@stormkit.io>
2022-10-23 23:23:51 +0200

    chore: use v2 components

--- src/components/CopyBox/CopyBox.tsx
+++ src/components/CopyBox/CopyBox.tsx
@@ -1,5 +1,6 @@
-import React, { FC, ReactElement } from "react";
-import Form from "~/components/Form";
+import React, { useState } from "react";
+import { Tooltip } from "@mui/material";
+import Form from "~/components/FormV2";
 import Button from "~/components/Button";
 
 type Props = {
@@ -8,31 +9,43 @@ type Props = {
 
 let id = 0;
 
-const CopyBox: FC<Props> = ({ value }: Props): ReactElement => {
+const CopyBox: React.FC<Props> = ({ value }: Props): React.ReactElement => {
+  const [clicked, setClicked] = useState(false);
   const inputId = `copy-token-${id++}`;
 
   return (
-    <>
-      <Form.Input
-        multiline
-        id={inputId}
-        value={value}
-        className="flex-auto"
-        inputProps={{
-          "aria-label": "Copy token",
-        }}
-      />
-      <Button
-        type="button"
-        onClick={() => {
-          (document.querySelector(`#${inputId}`) as HTMLInputElement).focus();
-          (document.querySelector(`#${inputId}`) as HTMLInputElement).select();
-          document.execCommand("copy");
-        }}
-      >
-        <span className="far fa-copy" />
-      </Button>
-    </>
+    <Form.Input
+      id={inputId}
+      value={value}
+      className="flex-auto"
+      aria-label="Copy content"
+      InputProps={{
+        endAdornment: (
+          <Tooltip open={clicked} title="Copied to clipboard">
+            <span>
+              <Button
+                type="button"
+                onClick={() => {
+                  (
+                    document.querySelector(`#${inputId}`) as HTMLInputElement
+                  ).focus();
+                  (
+                    document.querySelector(`#${inputId}`) as HTMLInputElement
+                  ).select();
+                  document.execCommand("copy");
+                  setClicked(true);
+                  setTimeout(() => {
+                    setClicked(false);
+                  }, 2000);
+                }}
+              >
+                <span className="far fa-copy text-white" />
+              </Button>
+            </span>
+          </Tooltip>
+        ),
+      }}
+    />
   );
 };
 
