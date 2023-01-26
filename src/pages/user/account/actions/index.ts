d0cf1b96984436b019ffc5f0e091e1f24c8e3405
Can Eldem <eldemcan@users.noreply.github.com>
2023-01-26 00:54:50 +0000

    chore: add delete user button

--- src/pages/user/account/actions/index.ts
+++ src/pages/user/account/actions/index.ts
@@ -7,3 +7,8 @@ export { useFetchSubscription } from "./fetch_subscriptions";
 export { useFetchCards } from "./fetch_cards";
 export { usePersonalAccessTokenState } from "./personal_access_token";
 export type Subscription = Stype;
+import api from "~/utils/api/Api";
+
+export const deleteUser = async () => {
+  return await api.delete(`/user`);
+}
