fbaf7dae8006e4b2fa3f62532c54441eb0c87d86
Savas Vedova <savas@stormkit.io>
2023-05-18 15:49:28 +0300

    chore: remove unused methods

--- src/pages/user/account/actions/index.ts
+++ src/pages/user/account/actions/index.ts
@@ -1,8 +1,6 @@
 import { Subscription as Stype } from "./fetch_subscriptions";
 
-export { handleRemovePaymentMethod } from "./handle_remove_payment_method";
 export { useFetchSubscription } from "./fetch_subscriptions";
-export { useFetchCards } from "./fetch_cards";
 export { usePersonalAccessTokenState } from "./personal_access_token";
 export type Subscription = Stype;
 import api from "~/utils/api/Api";
