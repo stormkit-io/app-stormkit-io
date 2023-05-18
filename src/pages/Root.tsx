5925a46290b69e7adc28bc138b9d0d2eef79001a
Savas Vedova <savas@stormkit.io>
2023-05-18 15:27:27 +0300

    feat: migrate to free trial accounts

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import { StyledEngineProvider } from "@mui/material/styles";
+import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import AuthContext from "./auth/Auth.context";
 import routes from "./routes";
+import theme from "./mui-theme";
 
 interface Props {
   Router: typeof BrowserRouter;
@@ -10,18 +11,20 @@ interface Props {
 
 const Root: React.FC<Props> = ({ Router }): React.ReactElement => (
   <StyledEngineProvider injectFirst>
-    <Router>
-      <AuthContext>
-        <Routes>
-          {routes.map(route => (
-            <Route
-              {...route}
-              key={Array.isArray(route.path) ? route.path[0] : route.path}
-            />
-          ))}
-        </Routes>
-      </AuthContext>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AuthContext>
+          <Routes>
+            {routes.map(route => (
+              <Route
+                {...route}
+                key={Array.isArray(route.path) ? route.path[0] : route.path}
+              />
+            ))}
+          </Routes>
+        </AuthContext>
+      </Router>
+    </ThemeProvider>
   </StyledEngineProvider>
 );
 
--- src/pages/user/account/actions/index.ts
+++ src/pages/user/account/actions/index.ts
@@ -1,8 +1,6 @@
 import { Subscription as Stype } from "./fetch_subscriptions";
 
 export { handleRemovePaymentMethod } from "./handle_remove_payment_method";
-export { handleUpdatePaymentMethod } from "./handle_update_payments";
-export { handleUpdateSubscriptionPlan } from "./handle_update_subscription_plan";
 export { useFetchSubscription } from "./fetch_subscriptions";
 export { useFetchCards } from "./fetch_cards";
 export { usePersonalAccessTokenState } from "./personal_access_token";
@@ -11,4 +9,4 @@ import api from "~/utils/api/Api";
 
 export const deleteUser = async () => {
   return await api.delete(`/user`);
-}
+};
