0515ae7cd63b87863975a1bbbbfb0d6201dbacc8
Can Eldem <eldemcan@users.noreply.github.com>
2023-11-18 21:42:39 +0000

    chore: fix canary redirection

--- src/utils/api/Api.ts
+++ src/utils/api/Api.ts
@@ -110,6 +110,7 @@ class Api {
     }
 
     if (
+      localStorage.getItem("sk_canary") == 'true' ||
       document?.cookie.indexOf("sk_canary=true") > -1 ||
       window.location.search?.indexOf("sk_canary=true") > -1
     ) {
