b71aab82f959b8f6f0717bc5c540dede776d7125
Savas Vedova <savas@stormkit.io>
2023-11-22 21:12:36 +0300

    chore: redesign deploy triggers

--- src/pages/apps/[id]/settings/_components/FormTriggerDeploys.tsx
+++ src/pages/apps/[id]/settings/_components/FormTriggerDeploys.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
-import cn from "classnames";
+import React, { FormEventHandler, useState } from "react";
+import Alert from "@mui/material/Alert";
 import Button from "@mui/lab/LoadingButton";
-import Form from "~/components/FormV2";
-import Link from "~/components/Link";
-import InfoBox from "~/components/InfoBoxV2";
+import Box from "@mui/material/Box";
+import Card from "~/components/Card";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import CardHeader from "~/components/CardHeader";
+import CardFooter from "~/components/CardFooter";
 import CopyBox from "~/components/CopyBox";
 import api from "~/utils/api/Api";
-import { updateDeployTrigger, deleteTrigger } from "../actions";
+import { deleteTrigger } from "../actions";
 import type { AppSettings } from "../types.d";
 
 interface Props {
@@ -23,98 +26,119 @@ const FormTriggerDeploys: React.FC<Props> = ({
   setAdditionalSettings,
 }): React.ReactElement => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string>("");
+
+  const handleSubmit: FormEventHandler<HTMLElement> = e => {
+    e.preventDefault();
+
+    setLoading(true);
+    setError("");
+
+    api
+      .put(`/app/deploy-trigger`, { appId: app.id })
+      .then(() => {
+        setLoading(false);
+        window.location.reload();
+      })
+      .catch(() => {
+        setLoading(false);
+        setError(
+          "Something went wrong while generating a new hash. Please try again and if the problem persists contact us on Discord or Email."
+        );
+      });
+  };
+
   return (
-    <>
-      <Form
-        handleSubmit={updateDeployTrigger({
-          app,
-          setLoading,
-          setError,
-        })}
-      >
+    <Card component="form" error={error} onSubmit={handleSubmit} sx={{ mb: 4 }}>
+      <CardHeader
+        title="Deploy triggers"
+        subtitle="Create endpoints to trigger deployments programmatically."
+      />
+      <Box sx={{ mb: 4 }}>
         {!additionalSettings.deployTrigger ? (
-          <InfoBox className="mx-4">
-            <p>
-              Click the Generate button to create an endpoint to trigger
-              deployments.{" "}
-              <Link
-                to="https://www.stormkit.io/docs/deployments#trigger"
-                secondary
-              >
+          <Alert color="info">
+            <Typography>
+              Click Generate button to create an endpoint for each environment
+              to trigger deployments programmatically.{" "}
+              <Link href="https://www.stormkit.io/docs/deployments#trigger">
                 Learn more
               </Link>
               .
-            </p>
-          </InfoBox>
+            </Typography>
+          </Alert>
         ) : (
-          environments.map((env, i) => (
-            <Form.WithLabel
-              key={env.id}
-              label={env.env}
-              className={cn("pb-0", { "pt-0": i === 0 })}
-              tooltip={
-                <p>
-                  A <b>POST</b> or <b>GET</b> request to this endpoint will
-                  trigger a deployment.
-                  <Link
-                    to="https://www.stormkit.io/docs/deployments#trigger"
-                    secondary
-                  >
-                    Learn more
-                  </Link>{" "}
-                </p>
-              }
-            >
-              {
+          <>
+            {environments.map((env, i) => (
+              <Box key={env.id} sx={{ mb: 4 }}>
+                <Typography sx={{ mb: 1 }}>{env.name}</Typography>
                 <CopyBox
                   value={`${api.url}/hooks/app/${app.id}/deploy/${additionalSettings.deployTrigger}/${env.id}`}
                 />
-              }
-            </Form.WithLabel>
-          ))
+              </Box>
+            ))}
+            <Alert color="info">
+              <Typography>
+                You can send a <b>POST</b> or <b>GET</b> request to initiate a
+                deployment for the given environment.{" "}
+                <Link href="https://www.stormkit.io/docs/deployments#trigger">
+                  Learn more
+                </Link>
+                .
+              </Typography>
+            </Alert>
+          </>
         )}
-        <div className="flex justify-end p-4">
-          {additionalSettings.deployTrigger && (
-            <Button
-              color="secondary"
-              variant="contained"
-              sx={{ mr: 2 }}
-              loading={loading}
-              onClick={e => {
-                e.preventDefault();
-                setLoading(true);
-                setAdditionalSettings({
-                  ...additionalSettings,
-                  deployTrigger: undefined,
-                });
-                deleteTrigger(app.id)
-                  .then(() => {
-                    setLoading(false);
-                  })
-                  .catch(e => {
-                    setError(e.message);
-                    setLoading(false);
-                  });
-              }}
-            >
-              Delete endpoints
-            </Button>
-          )}
+      </Box>
+      <CardFooter
+        sx={
+          additionalSettings.deployTrigger
+            ? {
+                display: "flex",
+                justifyContent: "space-between",
+              }
+            : {}
+        }
+      >
+        {additionalSettings.deployTrigger && (
           <Button
+            color="info"
+            variant="text"
+            sx={{ mr: 2 }}
             loading={loading}
-            type="submit"
-            color="secondary"
-            variant="contained"
+            onClick={e => {
+              e.preventDefault();
+              setLoading(true);
+
+              setAdditionalSettings({
+                ...additionalSettings,
+                deployTrigger: undefined,
+              });
+
+              deleteTrigger(app.id)
+                .then(() => {
+                  setLoading(false);
+                })
+                .catch(e => {
+                  setError(e.message);
+                  setLoading(false);
+                });
+            }}
           >
-            {additionalSettings.deployTrigger
-              ? "Generate a new endpoint"
-              : "Generate"}
+            Delete endpoints
           </Button>
-        </div>
-        {error && <InfoBox type={InfoBox.ERROR}>{error}</InfoBox>}
-      </Form>
-    </>
+        )}
+        <Button
+          loading={loading}
+          type="submit"
+          color="secondary"
+          variant="contained"
+        >
+          {additionalSettings.deployTrigger
+            ? "Generate a new endpoint"
+            : "Generate"}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
