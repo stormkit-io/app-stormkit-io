8d79febdc606eb4d611f1fbe1ee5771611d1dcd4
Savas Vedova <savas@stormkit.io>
2023-11-22 11:49:47 +0300

    chore: add team member interface

--- src/types/teams.d.ts
+++ src/types/teams.d.ts
@@ -1,7 +1,23 @@
+declare type TeamRole = "owner" | "admin" | "developer";
+
 declare interface Team {
   id: string;
   name: string;
   slug: string;
   isDefault: boolean;
-  currentUserRole: "owner" | "admin" | "developer";
+  currentUserRole: TeamRole;
+}
+
+declare interface TeamMember {
+  id: string;
+  userId: string;
+  teamId: string;
+  firstName?: string;
+  lastName?: string;
+  fullName?: string;
+  avatar?: string;
+  displayName: string;
+  email: string;
+  role: TeamRole;
+  status: boolean;
 }
