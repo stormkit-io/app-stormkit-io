2b2b9e6e61e64f7c1bf74ced7d0209741e8dc542
Savas Vedova <savas@stormkit.io>
2023-11-23 13:41:52 +0300

    chore: remove unused import

--- src/pages/apps/[id]/settings/_components/FormDangerZone.spec.tsx
+++ src/pages/apps/[id]/settings/_components/FormDangerZone.spec.tsx
@@ -1,6 +1,5 @@
 import type { RenderResult } from "@testing-library/react";
 import { waitFor, fireEvent, render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import mockApp from "~/testing/data/mock_app";
 import { mockDeleteApp } from "~/testing/nocks/nock_app";
 import FormDangerZone from "./FormDangerZone";
