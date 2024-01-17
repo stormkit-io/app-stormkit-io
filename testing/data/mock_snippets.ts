85d649f5b0e735bf103975c6ba672a846fd6c3a2
Savas Vedova <savas@stormkit.io>
2024-01-17 23:15:44 +0100

    chore: use new snippets api

--- testing/data/mock_snippets.ts
+++ testing/data/mock_snippets.ts
@@ -1,22 +1,16 @@
-interface SnippetResponse extends Omit<Snippet, "location"> {}
-
-export default (): { head: SnippetResponse[]; body: SnippetResponse[] } => ({
-  head: [
-    {
-      id: 1,
-      enabled: false,
-      prepend: false,
-      content: "<script>console.log('snippet 1')</script>",
-      title: "Snippet 1",
-    },
-  ],
-  body: [
-    {
-      id: 2,
-      enabled: true,
-      prepend: false,
-      content: "<script>console.log('snippet 2')</script>",
-      title: "Snippet 2",
-    },
-  ],
-});
+export default (): Snippet[] => [
+  {
+    id: 1,
+    enabled: false,
+    prepend: false,
+    content: "<script>console.log('snippet 1')</script>",
+    title: "Snippet 1",
+  },
+  {
+    id: 2,
+    enabled: true,
+    prepend: false,
+    content: "<script>console.log('snippet 2')</script>",
+    title: "Snippet 2",
+  },
+];
