4cd543ee3d0ad41651562196e3e8cc804fa7a209
Can Eldem <eldemcan@users.noreply.github.com>
2024-02-16 10:41:42 +0000

    chore: add txt lookup value as label

--- src/pages/apps/[id]/environments/[env-id]/config/_components/TabDomainConfig/DomainVerifyModal.tsx
+++ src/pages/apps/[id]/environments/[env-id]/config/_components/TabDomainConfig/DomainVerifyModal.tsx
@@ -11,6 +11,7 @@ import Card from "~/components/Card";
 import CardRow from "~/components/CardRow";
 import CopyBox from "~/components/CopyBox";
 import { useDomainLookup } from "./actions";
+import { Tooltip } from "@mui/material";
 
 interface Props {
   domain: Domain;
@@ -57,6 +58,21 @@ const DomainVerificationStatus: React.FC<Props> = ({
                 color={info.dns.verified ? "success" : undefined}
                 sx={{ color: "white" }}
                 label={info.dns.verified ? "verified" : "not yet verified"}
+                size="small"
+              />
+            </CardRow>
+            <CardRow>
+              <Box sx={{ display: "flex", gap: "10px" }}>
+                <Typography sx={{ color: grey[500], mb: 2 }}>
+                  TXT Lookup Value
+                </Typography>
+                <Tooltip placement="top" title="You can verify if the TXT value is propagated from https://mxtoolbox.com/txtlookup.aspx">
+                  <span className="fas fa-question-circle" />
+                </Tooltip>
+              </Box>
+              <Chip
+                sx={{ color: "white" }}
+                label={info.dns.txt.lookup}
                 size="small"
               />
             </CardRow>
