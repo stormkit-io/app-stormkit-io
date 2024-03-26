f6ff06775cb08dc153165b067b05d4d92bc612b8
Can Eldem <eldemcan@users.noreply.github.com>
2024-03-26 21:49:06 +0000

    chore: update

--- src/utils/api/helpers.ts
+++ src/utils/api/helpers.ts
@@ -13,8 +13,8 @@ export function prepareHeaders(token: string) {
   });
 }
 
-export function isOnPrem(url:string = window.location.href): boolean {
-  const regex = /^https?:\/\/(?:.*\.)?stormkit\.io(?:\/|$)/i;
+export function isOnPrem(url: string = window.location.href): boolean {
+  const regex = /^https?:\/\/(?:.*\.)?(stormkit\.io|stormkit\.dev)(?:\/|$)/i;
   return !regex.test(url);
 }
 
