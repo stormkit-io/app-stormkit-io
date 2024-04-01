8d0635651f2d1c2fe72512e3eb4d28bbb7df71b7
Can Eldem <eldemcan@users.noreply.github.com>
2024-04-01 20:18:16 +0100

    chore: show error message and empty screen

--- src/pages/apps/[id]/environments/[env-id]/kv/Kv.tsx
+++ src/pages/apps/[id]/environments/[env-id]/kv/Kv.tsx
@@ -15,7 +15,7 @@ import {
   Alert,
   LinearProgress,
 } from "@mui/material";
-import React, { useContext, useState } from "react";
+import  { useContext, useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { AppContext } from "~/pages/apps/[id]/App.context";
 import { EnvironmentContext } from "~/pages/apps/[id]/environments/Environment.context";
@@ -26,6 +26,7 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import { json } from "@codemirror/lang-json";
 import { instanceOf } from "prop-types";
+import EmptyPage from "~/components/EmptyPage";
 
 export default function KV() {
   const { app } = useContext(AppContext);
@@ -46,14 +47,43 @@ export default function KV() {
   -d '["SET", "myKey", "Hello, Redis!"]'`;
 
   const handleViewClick = (key: string) => {
-    FetchValue({ appId: app.id, environmentId: environment.id, key: key }).then((res) => {
-      if (typeof res === 'string') {  // Use typeof for type checking strings
-        setViewData(res);
-      } else {
-        setViewData(JSON.stringify(res, null, 2) );
+    FetchValue({ appId: app.id, environmentId: environment.id, key: key }).then(
+      res => {
+        if (typeof res === "string") {
+          // Use typeof for type checking strings
+          setViewData(res);
+        } else {
+          setViewData(JSON.stringify(res, null, 2));
+        }
       }
-    });
+    );
     setSelectedKey(key);
+  };
+
+  const renderDataViewer = () => {
+    return (
+      <Box>
+        {viewData ? (
+          <>
+            <Box sx={{ marginLeft: "10px" }}>
+              <CodeMirror
+                value={viewData}
+                extensions={[json()]}
+                theme="dark"
+                readOnly={true}
+                minWidth="100%"
+                maxWidth="100%"
+              />
+            </Box>
+          </>
+        ) : (
+          <Box display={"grid"} sx={{ placeItems: "center", color: "white" }}>
+            <ManageSearchIcon style={{ fontSize: "2rem" }} />
+            <Typography>Select a key from the list</Typography>
+          </Box>
+        )}
+      </Box>
+    );
   };
 
   return (
@@ -79,7 +109,6 @@ export default function KV() {
             extensions={[json()]}
             theme="dark"
             readOnly={true}
-            // options={{lineWrapping: true}}
             value={curlCommand}
             minWidth="100%"
             maxWidth="100%"
@@ -92,103 +121,87 @@ export default function KV() {
 
       {loading && <LinearProgress color="secondary" />}
       {error && (
-        <Alert color="error">
-          <Box>{error}</Box>
-        </Alert>
+        <Box marginBottom={1}>
+          <Alert color="error">
+            <Box>{error}</Box>
+          </Alert>
+        </Box>
       )}
-      <Grid container>
-        <Grid item xs={6}>
-          <TableContainer
-            style={{
-              height: "400px",
-              overflowY: "auto",
-              backgroundColor: "rgba(0,0,0,0.3)",
-            }}
-          >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Keys</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {keys.map((key, idx) => (
-                  <TableRow
-                    sx={{
-                      "&:hover": {
-                        backgroundColor: "rgba(255, 255, 255, 0.1)",
-                      },
-                      borderBottom: "1px dotted black",
-                      backgroundColor:
-                        selectedKey == key
-                          ? "rgba(255, 255, 255, 0.1)"
-                          : "transparent",
-                    }}
-                    key={`${idx}row`}
-                    onClick={() => handleViewClick(key)}
-                  >
-                    <TableCell>
-                      <Box
-                        key={idx}
-                        sx={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                          alignItems: "center",
-                        }}
-                      >
+
+      {keys.length == 0 ? (
+        <EmptyPage>No key is set yet</EmptyPage>
+      ) : (
+        <Grid container>
+          <Grid item xs={6}>
+            <TableContainer
+              style={{
+                height: "400px",
+                overflowY: "auto",
+                backgroundColor: "rgba(0,0,0,0.3)",
+              }}
+            >
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Keys</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {keys.map((key, idx) => (
+                    <TableRow
+                      sx={{
+                        "&:hover": {
+                          backgroundColor: "rgba(255, 255, 255, 0.1)",
+                        },
+                        borderBottom: "1px dotted black",
+                        backgroundColor:
+                          selectedKey == key
+                            ? "rgba(255, 255, 255, 0.1)"
+                            : "transparent",
+                      }}
+                      key={`${idx}row`}
+                      onClick={() => handleViewClick(key)}
+                    >
+                      <TableCell>
                         <Box
+                          key={idx}
                           sx={{
                             display: "flex",
                             justifyContent: "space-between",
                             alignItems: "center",
-                            overflow: "hidden",
-                            textOverflow: "ellipsis",
-                            whiteSpace: "nowrap",
                           }}
                         >
-                          {key}
+                          <Box
+                            sx={{
+                              display: "flex",
+                              justifyContent: "space-between",
+                              alignItems: "center",
+                              overflow: "hidden",
+                              textOverflow: "ellipsis",
+                              whiteSpace: "nowrap",
+                            }}
+                          >
+                            {key}
+                          </Box>
+                          <Box sx={{ alignItems: "right" }}>
+                            <Button sx={{ color: "white" }}>
+                              <ArrowForwardIosIcon fontSize="small" />
+                            </Button>
+                          </Box>
                         </Box>
-                        <Box sx={{ alignItems: "right" }}>
-                          <Button sx={{ color: "white" }}>
-                            <ArrowForwardIosIcon fontSize="small" />
-                          </Button>
-                        </Box>
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Grid>
 
-        <Grid item xs={6}>
-          <Box>
-            {viewData ? (
-              <>
-                <Box sx={{ marginLeft: "10px" }}>
-                  <CodeMirror
-                    value={viewData}
-                    extensions={[json()]}
-                    theme="dark"
-                    readOnly={true}
-                    minWidth="100%"
-                    maxWidth="100%"
-                  />
-                </Box>
-              </>
-            ) : (
-              <Box
-                display={"grid"}
-                sx={{ placeItems: "center", color: "white" }}
-              >
-                <ManageSearchIcon style={{ fontSize: "2rem" }} />
-                <Typography>Select a key from the list</Typography>
-              </Box>
-            )}
-          </Box>
+          <Grid item xs={6}>
+            {renderDataViewer()}
+          </Grid>
         </Grid>
-      </Grid>
+      )}
     </>
   );
 }
