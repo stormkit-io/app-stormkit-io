095a9ca630ae9f211b121cae04c2948bbaa47aa8
Can Eldem <eldemcan@users.noreply.github.com>
2024-05-28 20:10:44 +0100

    remove sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,5 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import Root from "~/pages/Root";
 
-if (process.env.SENTRY) {
-  Promise.all([import("@sentry/react"), import("@sentry/tracing")]).then(
-    ([Sentry, { Integrations }]) => {
-      Sentry.init({
-        dsn: process.env.SENTRY,
-        integrations: [new Integrations.BrowserTracing()],
-        tracesSampleRate: 1.0,
-      });
-    }
-  );
-}
-
 const root = createRoot(document.querySelector("#root")!);
 root.render(<Root Router={BrowserRouter} />);
