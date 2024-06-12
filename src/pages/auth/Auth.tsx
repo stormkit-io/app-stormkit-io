9366b4c5b61ad0fc6496a415531e74ceec744720
Savas Vedova <savas@stormkit.io>
2024-06-12 16:30:14 +0300

    chore: improve styling for centering component

--- src/pages/auth/Auth.tsx
+++ src/pages/auth/Auth.tsx
@@ -51,99 +51,107 @@ export default function Auth() {
   return (
     <Box
       sx={{
-        flexDirection: "column",
-        position: "relative",
-        top: { lg: -40 },
-        maxWidth: "62rem",
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <Box sx={{ mb: 8, display: "flex", justifyContent: "center" }}>
-        <Logo iconSize={150} />
-      </Box>
       <Box
         sx={{
-          display: "flex",
-          color: "white",
-          flexDirection: { xs: "column-reverse", lg: "row" },
-          px: { xs: 1, lg: 3 },
+          flexDirection: "column",
+          position: "relative",
+          maxWidth: "62rem",
         }}
       >
+        <Box sx={{ mb: 8, display: "flex", justifyContent: "center" }}>
+          <Logo iconSize={150} />
+        </Box>
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
-            mr: { lg: 8 },
-            mb: { xs: 3, md: 0 },
-            mt: { xs: 4, lg: 0 },
+            color: "white",
+            flexDirection: { xs: "column-reverse", lg: "row" },
             px: { xs: 1, lg: 3 },
-            pt: { xs: 0, lg: 6 },
-            maxWidth: { sm: "28rem" },
           }}
         >
-          <Typography sx={{ lineHeight: 2 }}>
-            /def/{" "}
-            <Typography component="span" color="secondary.dark">
-              Noun.
-            </Typography>
-            <br />
-            1. Serverless app development platform.
-            <br />
-            2. A set of tools built to save dev-ops time for your Javascript
-            application.
-          </Typography>
-          <Box component="ul" sx={{ mt: 4, lineHeight: 2 }}>
-            <Box component="li">
-              <i className="fas fa-undo-alt mr-4" /> Environments with instant
-              rollbacks
-            </Box>
-            <Box component="li">
-              <i className="fas fa-shield-alt mr-4" /> Custom domains &amp;
-              automated SSL
-            </Box>
-            <Box component="li">
-              <i className="fas fa-cloud mr-3" /> Serverless functions
-            </Box>
-          </Box>
-        </Box>
-        <Card
-          sx={{
-            px: 3,
-            maxWidth: { sm: "28rem" },
-            minWidth: { sm: "28rem" },
-          }}
-          error={authError}
-          loading={loading}
-        >
-          <CardHeader
-            title="Authentication"
-            subtitle="Log in with your provider"
-            sx={{ textAlign: "center" }}
-          />
           <Box
             sx={{
               display: "flex",
               flexDirection: "column",
-              px: 4,
-              mb: authError ? 4 : 2,
+              mr: { lg: 8 },
+              mb: { xs: 3, md: 0 },
+              mt: { xs: 4, lg: 0 },
+              px: { xs: 1, lg: 3 },
+              pt: { xs: 0, lg: 6 },
+              maxWidth: { sm: "28rem" },
             }}
           >
-            {providers?.github && (
-              <GithubButton
-                sx={{ mb: providers?.gitlab || providers.bitbucket ? 4 : 0 }}
-                onClick={() => loginOauth?.("github")}
-              />
-            )}
-            {providers?.gitlab && (
-              <GitlabButton
-                sx={{ mb: providers?.bitbucket ? 4 : 0 }}
-                onClick={() => loginOauth?.("gitlab")}
-              />
-            )}
-            {providers?.bitbucket && (
-              <BitbucketButton onClick={() => loginOauth?.("bitbucket")} />
-            )}
+            <Typography sx={{ lineHeight: 2 }}>
+              /def/{" "}
+              <Typography component="span" color="secondary.dark">
+                Noun.
+              </Typography>
+              <br />
+              1. Serverless app development platform.
+              <br />
+              2. A set of tools built to save dev-ops time for your Javascript
+              application.
+            </Typography>
+            <Box component="ul" sx={{ mt: 4, lineHeight: 2 }}>
+              <Box component="li">
+                <i className="fas fa-undo-alt mr-4" /> Environments with instant
+                rollbacks
+              </Box>
+              <Box component="li">
+                <i className="fas fa-shield-alt mr-4" /> Custom domains &amp;
+                automated SSL
+              </Box>
+              <Box component="li">
+                <i className="fas fa-cloud mr-3" /> Serverless functions
+              </Box>
+            </Box>
           </Box>
-        </Card>
+          <Card
+            sx={{
+              px: 3,
+              maxWidth: { sm: "28rem" },
+              minWidth: { sm: "28rem" },
+            }}
+            error={authError}
+            loading={loading}
+          >
+            <CardHeader
+              title="Authentication"
+              subtitle="Log in with your provider"
+              sx={{ textAlign: "center" }}
+            />
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                px: 4,
+                mb: authError ? 4 : 2,
+              }}
+            >
+              {providers?.github && (
+                <GithubButton
+                  sx={{ mb: providers?.gitlab || providers.bitbucket ? 4 : 0 }}
+                  onClick={() => loginOauth?.("github")}
+                />
+              )}
+              {providers?.gitlab && (
+                <GitlabButton
+                  sx={{ mb: providers?.bitbucket ? 4 : 0 }}
+                  onClick={() => loginOauth?.("gitlab")}
+                />
+              )}
+              {providers?.bitbucket && (
+                <BitbucketButton onClick={() => loginOauth?.("bitbucket")} />
+              )}
+            </Box>
+          </Card>
+        </Box>
       </Box>
     </Box>
   );
