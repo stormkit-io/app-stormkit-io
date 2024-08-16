2a386362b90acc356c0ada7350a301691ab24de3
Savas Vedova <savas@stormkit.io>
2024-08-16 10:46:10 +0300

    chore: allow passing sx properties to overwrite styles

--- src/components/Dot/Dot.tsx
+++ src/components/Dot/Dot.tsx
@@ -1,8 +1,13 @@
+import type { SxProps } from "@mui/material";
 import Box from "@mui/material/Box";
 
-export default function Dot() {
+interface Props {
+  sx?: SxProps;
+}
+
+export default function Dot({ sx }: Props) {
   return (
-    <Box component="span" sx={{ mx: 0.5, opacity: 0.5 }}>
+    <Box component="span" sx={{ mx: 0.5, opacity: 0.5, ...sx }}>
       &#183;
     </Box>
   );
