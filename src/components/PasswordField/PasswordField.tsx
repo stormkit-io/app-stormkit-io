18376640ed4c87294ce5bb8df9a4fc692c4450fa
Savas Vedova <savas@stormkit.io>
2025-10-12 23:07:57 +0200

    refactor: migrate deprecated props

--- src/components/PasswordField/PasswordField.tsx
+++ src/components/PasswordField/PasswordField.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import TextField, { BaseTextFieldProps } from "@mui/material/TextField";
+import TextField, { TextFieldProps } from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
-interface Props extends BaseTextFieldProps {
+type Props = TextFieldProps & {
   ariaLabel?: string;
   endAdornment?: React.ReactNode;
   defaultVisible?: boolean;
-}
+};
 
 const fontSize = 14;
 
@@ -26,30 +26,32 @@ export default function PasswordField({
       autoComplete="off"
       type={isValueVisible ? "text" : "password"}
       {...props}
-      inputProps={{
-        "data-1p-ignore": "true",
-        "aria-label": ariaLabel,
-      }}
-      InputProps={{
-        endAdornment: (
-          <>
-            <IconButton
-              sx={{ width: 24, height: 24 }}
-              type="button"
-              aria-label={`Toggle ${props.name} visibility`}
-              onClick={() => {
-                setIsValueVisible(!isValueVisible);
-              }}
-            >
-              {isValueVisible ? (
-                <VisibilityOffIcon sx={{ fontSize }} />
-              ) : (
-                <VisibilityIcon sx={{ fontSize }} />
-              )}
-            </IconButton>
-            {endAdornment}
-          </>
-        ),
+      slotProps={{
+        input: {
+          // @ts-ignore
+          "data-1p-ignore": "true",
+          "aria-label": ariaLabel,
+          endAdornment: (
+            <>
+              <IconButton
+                sx={{ width: 24, height: 24 }}
+                type="button"
+                aria-label={`Toggle ${props.name} visibility`}
+                onClick={() => {
+                  setIsValueVisible(!isValueVisible);
+                }}
+              >
+                {isValueVisible ? (
+                  <VisibilityOffIcon sx={{ fontSize }} />
+                ) : (
+                  <VisibilityIcon sx={{ fontSize }} />
+                )}
+              </IconButton>
+              {endAdornment}
+            </>
+          ),
+        },
+        ...props?.slotProps,
       }}
     />
   );
